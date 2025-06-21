f4724811631b9bf59dc6ed28ce0037e2d758733b
Yusuff <steve.elegbede@gmail.com>
2025-06-21 01:20:15 -0400

    2nd push

--- destroy.sh
+++ destroy.sh
@@ -11,7 +11,7 @@
 pushd terraform-s3-cloudfront
 
 # Get the bucket name from Terraform output
-BUCKET_NAME=$(terraform output -raw s3_bucket_name)
+# BUCKET_NAME=$(terraform output -raw s3_bucket_name)
 
 # Empty the S3 bucket before destroying resources
 aws s3 rm s3://$BUCKET_NAME --recursive
